bba841598bdf598ebb0d38235912b7865011bef3
unacceptableUse <unacceptableuse@gmail.com>
2016-09-24 14:12:24 +0100

    Made it actually work

--- modules/petify-import.js
+++ modules/petify-import.js
@@ -4,7 +4,7 @@
 
 
 var r = require('rethinkdb');
-
+var uuid = require('uuid').v4;
 
 module.exports = function(app){
     console.log("Starting petify 1 import...");
@@ -13,7 +13,7 @@ module.exports = function(app){
         db: '',
         user: '',
         password: ''
-    }, function(err, conn){
+    }, function(err, rconn){
         if(err){
             console.error("Error connecting to rethinkdb: "+err);
         }else{
@@ -21,29 +21,35 @@ module.exports = function(app){
                 if(err){
                     console.error("Error getting all songs: "+err);
                 } else{
-                    cursor.each(function(err, song){
+                    cursor.eachAsync(function(song, cb){
                         if(err){
                             console.error("Error during cursor foreach: "+err);
                         }else{
-                                app.database.getOrCreateAlbum(song.album ? song.album : "Unknown Album", function(err, albumID){
-                                    app.database.getOrCreateArtist(song.artist ? song.artist : "Unknown Artist", function(err, artistID){
+                            app.database.getOrCreateAlbum(song.album ? song.album.trim() : "Unknown Album", function(err, albumID){
+                                app.database.getOrCreateArtist(song.artist ? song.artist.trim() : "Unknown Artist", function(err, artistID){
+                                    app.database.getOrCreateGenre(song.catagories[0] ? song.catagories[0] : "Unknown", function(err, genreID){
                                         app.database.addSong({
+                                            id: uuid(),
                                             artist: artistID,
                                             album: albumID,
                                             mbid: song.mbid,
                                             duration: song.duration,
                                             addedby: "e110a6dc-8254-11e6-839f-00224dae0d2a",
-                                            genre: null,
+                                            path: song.path,
+                                            genre: genreID,
                                             title: song.title
                                         }, function(err, res){
                                             if(err){
                                                 console.error("ERROR ADDING SONG "+song.title+": "+err);
+                                                cb();
                                             }else{
                                                 console.log("Added song "+song.title);
+                                                cb();
                                             }
                                         });
                                     });
                                 });
+                            });
                         }
                     });
                 }
