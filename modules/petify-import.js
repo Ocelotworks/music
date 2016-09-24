85f4360c4a28bdc8494e8840e528596be8df7078
unacceptableUse <unacceptableuse@gmail.com>
2016-09-24 20:21:59 +0100

    Updated to get album art

--- modules/petify-import.js
+++ modules/petify-import.js
@@ -17,7 +17,8 @@ module.exports = function(app){
         if(err){
             console.error("Error connecting to rethinkdb: "+err);
         }else{
-            r.table("music").without("albumArt").run(rconn, function(err, cursor){
+            console.log("Getting all the albums... This will take a while");
+            r.table("music").pluck("album", "albumArt").distinct().run(rconn, function(err, cursor){
                 if(err){
                     console.error("Error getting all songs: "+err);
                 } else{
@@ -25,31 +26,39 @@ module.exports = function(app){
                         if(err){
                             console.error("Error during cursor foreach: "+err);
                         }else{
-                            app.database.getOrCreateAlbum(song.album ? song.album.trim() : "Unknown Album", function(err, albumID){
-                                app.database.getOrCreateArtist(song.artist ? song.artist.trim() : "Unknown Artist", function(err, artistID){
-                                    app.database.getOrCreateGenre(song.catagories[0] ? song.catagories[0] : "Unknown", function(err, genreID){
-                                        app.database.addSong({
-                                            id: uuid(),
-                                            artist: artistID,
-                                            album: albumID,
-                                            mbid: song.mbid,
-                                            duration: song.duration,
-                                            addedby: "e110a6dc-8254-11e6-839f-00224dae0d2a",
-                                            path: song.path,
-                                            genre: genreID,
-                                            title: song.title
-                                        }, function(err, res){
-                                            if(err){
-                                                console.error("ERROR ADDING SONG "+song.title+": "+err);
-                                                cb();
-                                            }else{
-                                                console.log("Added song "+song.title);
-                                                cb();
-                                            }
-                                        });
-                                    });
-                                });
+
+                            app.database.getOrCreateAlbum(song.album, function(err, albumID){
+                               app.database.updateAlbumArt(albumID, song.albumArt, function(err, res){
+                                   console.log("Done "+song.album);
+                                  cb();
+                               });
                             });
+
+                            //app.database.getOrCreateAlbum(song.album ? song.album.trim() : "Unknown Album", function(err, albumID){
+                            //    app.database.getOrCreateArtist(song.artist ? song.artist.trim() : "Unknown Artist", function(err, artistID){
+                            //        app.database.getOrCreateGenre(song.catagories[0] ? song.catagories[0] : "Unknown", function(err, genreID){
+                            //            app.database.addSong({
+                            //                id: uuid(),
+                            //                artist: artistID,
+                            //                album: albumID,
+                            //                mbid: song.mbid,
+                            //                duration: song.duration,
+                            //                addedby: "e110a6dc-8254-11e6-839f-00224dae0d2a",
+                            //                path: song.path,
+                            //                genre: genreID,
+                            //                title: song.title
+                            //            }, function(err, res){
+                            //                if(err){
+                            //                    console.error("ERROR ADDING SONG "+song.title+": "+err);
+                            //                    cb();
+                            //                }else{
+                            //                    console.log("Added song "+song.title);
+                            //                    cb();
+                            //                }
+                            //            });
+                            //        });
+                            //    });
+                            //});
                         }
                     });
                 }
