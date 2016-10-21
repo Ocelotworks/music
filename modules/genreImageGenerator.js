0e96f920030aa62d254f652840aab227ad804381
unacceptableUse <unacceptableuse@gmail.com>
2016-10-21 10:01:36 +0100

    Prettied up genreImageGenerator

--- modules/genreImageGenerator.js
+++ modules/genreImageGenerator.js
@@ -11,19 +11,24 @@ var path    = require('path');
 
 module.exports = function(app){
     return {
-        generateImageForGenre: function (genre) {
+        /**
+         * Creates a 300x300 square of 4 images from 4 random albums with songs with the specified genre on
+         * @param genre The genre UUID
+         */
+        generateImageForGenre: function generateImageForGenre(genre) {
             if (!fs.existsSync(config.get("General.tempDir"))) {
                 fs.mkdirSync(config.get("General.tempDir"));
             }
-            app.database.getAlbumArtForGenreImage(genre, function (err, data) {
+            app.database.getAlbumArtForGenreImage(genre, function getAlbumArtForGenreImage(err, data) {
                 if (err) {
                     app.error("Error getting album art for genre " + genre);
                 } else {
                     if (data.length < 4) {
-                        app.log("Not enough albums to create genre image for " + genre);
+                        app.warn("Not enough albums to create genre image for " + genre);
                     } else {
+                        app.log("Creating genre image for "+genre);
                         var imagePaths = [];
-                        async.each(data, function (imageBlob, cb) {
+                        async.each(data, function writeTempImages(imageBlob, cb) {
                             var imagePath = path.join(config.get("General.tempDir"), "image-" + genre + "-" + parseInt(Math.random() * 1000) + ".png");
                             imagePaths.push(imagePath);
                             fs.writeFile(imagePath, imageBlob.image, cb);
@@ -43,18 +48,18 @@ module.exports = function(app){
                                         .in(imagePaths[3])
                                         .minify()
                                         .mosaic()
-                                        .toBuffer('PNG', function (err, buffer) {
+                                        .toBuffer('PNG', function createImageBuffer(err, buffer) {
                                             if (err) {
-                                                app.error("Error writing genre image: " + err);
+                                                app.error("Error creating genre image buffer: " + err);
                                             } else {
-                                                app.database.putGenreImage(genre, buffer, function (err) {
+                                                app.database.putGenreImage(genre, buffer, function putGenreImage(err) {
                                                     if (err) {
                                                         app.error("Error putting genre image in database: " + err);
                                                     } else {
                                                         app.log("Updated genre image for " + genre);
                                                         for(var i in imagePaths) {
                                                             if (imagePaths.hasOwnProperty(i)) {
-                                                                fs.unlink(imagePaths[i], function (err) {
+                                                                fs.unlink(imagePaths[i], function deleteTempImages(err) {
                                                                     if (err)
                                                                         app.warn("Error deleting " + imagePaths[i] + ": " + err)
                                                                 });
