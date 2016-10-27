be355b4fb7f9546ea69aaca66eb6fb91a959ca67
unacceptableUse <unacceptableuse@gmail.com>
2016-10-27 18:21:08 +0100

    Made alerts work

--- client/admin/AdminController.js
+++ client/admin/AdminController.js
@@ -2,7 +2,7 @@
  * Copyright Ocelotworks 2016
  */
 
-app.controller("AdminController", function($scope, $rootScope, $sce, $templateRequest, $compile){
+app.controller("AdminController", function($scope, $rootScope, $sce, $templateRequest, $compile, $http){
 
     $scope.showErrorScreen = function(error){
         $scope.error = error;
@@ -22,6 +22,21 @@ app.controller("AdminController", function($scope, $rootScope, $sce, $templateRe
         $templateRequest(templateUrl).then(function(template){
             $compile($("#tabContainer").html(template).contents())($scope);
         }, $scope.showErrorScreen);
-    }
+    };
+
+
+    $scope.alert = {
+        url: base+"img/album.png",
+        users: {},
+        title: "",
+        body: "",
+        lifetime: 5
+    };
+
+    $scope.sendAlert = function(){
+        $http.post(base+'templates/admin/alerts', $scope.alert).then(function(resp){
+            $scope.error = resp.data.error;
+        });
+    };
 
 });
--- routes/templates/admin.js
+++ routes/templates/admin.js
@@ -28,5 +28,32 @@ module.exports = function(app){
         res.render('templates/admin/alerts', {layout: false, onlineUsers: app.expressWs.getWss('/ws/updates/').clients});
     });
 
+    router.post('/alerts', function(req, res){
+       if(!req.user || req.user.userlevel < 2){
+           res.header(401).json({error: "You don't have permission to do that."});
+       }else{
+            if(!req.body || !req.body.title || !req.body.body){
+                res.header(406).json({error: "A required field is missing."});
+            }else{
+                var message = {
+                    lifetime: req.body.lifetime,
+                    title: req.body.title,
+                    body: req.body.body,
+                    image: req.body.url || "img/album.png"
+                };
+                if(!req.body.users || req.body.users.length === 0 || !req.body.specificUsers){
+                    app.broadcastUpdate("alert", message);
+                }else{
+                    var clients = app.expressWs.getWss('/ws/updates/').clients;
+                    for(var i in req.body.users)
+                        if(req.body.users.hasOwnProperty(i) && req.body.users[i] && clients[i])
+                            clients[i].send(JSON.stringify({type: "alert", message: message}));
+                }
+                res.header(204);
+            }
+       }
+
+    });
+
     return router;
 };
