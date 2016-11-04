9ec8fb1352b7900f83412f663e73799c695c53a0
unacceptableUse <unacceptableuse@gmail.com>
2016-11-04 18:01:59 +0000

    Fixed not working on firefox

--- client/RootScope.js
+++ client/RootScope.js
@@ -194,7 +194,7 @@ app.run(['$rootScope', function($rootScope){
     $rootScope.playByElement = function playByElement(element){
         if(!element)return console.warn("Warning: playByElement called with a null element! Bad ID somewhere?");
         $(".songRate").removeClass("rated");
-        var info = element.outerText.split("\u00A0-\u00A0");
+        var info = (element.outerText || element.textContent).split("\u00A0-\u00A0");
         $rootScope.nowPlaying.id = element.attributes["data-id"].value;
         $rootScope.nowPlaying.artistID = element.attributes["data-artist"].value;
         $rootScope.nowPlaying.artist = info[0];
