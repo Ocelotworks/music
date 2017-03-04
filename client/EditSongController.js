52026ef51df4bae26f1400f34178e0bfd3a805a6
unacceptableUse <unacceptableuse@gmail.com>
2017-03-04 16:14:11 +0000

    Updated client script for song editing

--- client/EditSongController.js
+++ client/EditSongController.js
@@ -7,7 +7,7 @@ app.controller("EditSongController", function($scope, $rootScope, $http){
     $scope.albumSuggestions = [];
     $scope.artistSuggestions = [];
 
-
+    $scope.error = "";
 
     $scope.song = {
         album: $("#newAlbumName").val(),
@@ -15,15 +15,27 @@ app.controller("EditSongController", function($scope, $rootScope, $http){
         title: $("#newSongName").val(),
     };
 
-    $scope.editSong = function(){
-
-    };
-
     $scope.updateAndUnfocus = function(thing, value){
         $scope.song[thing] = value;
         $scope.albumSuggestions = [];
         $scope.artistSuggestions = [];
     };
+
+    $scope.swap = function(){
+        var artist = $scope.song.artist;
+        $scope.song.artist = $scope.song.title;
+        $scope.song.title = artist;
+    };
+
+    $scope.displayArtistSuggestions = debounce(function(){
+        if($scope.song.album) {
+            $http.get(base + "search/artist/" + $scope.song.artist).then(function (response) {
+                if (response.data){
+                    $scope.artistSuggestions = response.data;
+                }
+            });
+        }
+    }, 150);
 
 
     $scope.displayAlbumSuggestions = debounce(function(){
@@ -35,4 +47,14 @@ app.controller("EditSongController", function($scope, $rootScope, $http){
             });
         }
     }, 150);
+
+    $scope.editSong = function(){
+      $http.post(base+"api/song/"+$("#songInfoContainer").data("id")+"/update", $scope.song).then(function(response){
+         if(response.status == 200){
+            $scope.error = "Saved. Changes may not appear until the page is refreshed."
+         }else{
+             $scope.error = response.data && response.data.err ? response.data.err : "HTTP Error "+response.status;
+         }
+      });
+    };
 });
