551c2ec4a56dc760778da015a62cfa7b4bae6d6e
unacceptableuse <unacceptableuse@gmail.com>
2017-09-16 12:50:48 +0100

    Fix some images *still* showing christmas versions

--- routes/image.js
+++ routes/image.js
@@ -17,7 +17,7 @@ module.exports = function(app){
     router.get('/album/:id', function(req, res){
         app.database.getAlbumArt(req.params.id, function(err, resp){
             if(err || !resp[0] || !resp[0].image)
-                res.redirect("../../img/album-christmas.png");
+                res.redirect(app.christmasMode ? "../../img/album-christmas.png" : "../../img/album.png");
             else{
                 res.header('Content-Type', 'image/png');
                 res.end(resp[0].image);
@@ -28,7 +28,7 @@ module.exports = function(app){
     router.get('/artist/:id', function(req, res){
         app.database.getArtistImage(req.params.id, function(err, resp){
             if(err || !resp[0] || !resp[0].image) {
-                res.redirect("../../img/album-christmas.png");
+                res.redirect(app.christmasMode ? "../../img/album-christmas.png" : "../../img/album.png");
             }else{
                 res.header('Content-Type', 'image/png');
                 res.end(resp[0].image);
